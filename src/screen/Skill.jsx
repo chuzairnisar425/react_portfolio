cf60435fb60f61a90c70c4ed6b0acc2bccc8f790
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-28 10:54:49 +0500

    modified

--- src/screen/Skill.jsx
+++ src/screen/Skill.jsx
@@ -7,16 +7,17 @@ export const Skill = () => {
         <div className="container">
           <div className="section-header text-center">
             <h2 className="section-title">My Skills</h2>
-            <p>We put your ideas and thus your wishes in the form of a unique web project that inspires you and your customers.</p>
+            <p>
+              We put your ideas and thus your wishes in the form of a unique web
+              project that inspires you and your customers.
+            </p>
           </div>
 
           <div className="skills-widget grid6">
             {skillsData.map((skill) => (
               <div className="skill-item" key={skill.id}>
-                <div className="skill-inner">
-                  <div className="icon-box">
-                    <img src={skill.icon} alt={skill.name} />
-                  </div>
+                <div className="icon-box">
+                  <img src={skill.icon} alt={skill.name} />
                 </div>
                 <p>{skill.name}</p>
               </div>
